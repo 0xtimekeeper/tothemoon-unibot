22294a764f8abb54d6e2361b5773d3162267d6e2
0xtimekeeper <164117581+0xtimekeeper@users.noreply.github.com>
2024-03-20 13:23:09 +1100

    Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# tothemoon-unibot
+
+Take investments to the moon with UniBot, a powerful bot for managing portfolios.
+
+## Installation
+
+```bash
+git clone https://github.com/0xtimekeeper/tothemoon-unibot.git
+cd tothemoon-unibot
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+const tothemoon_unibot = require('tothemoon-unibot');
+const tothemoon_unibot_deploy = require('tothemoon-unibot-deploy');
+const fs_extra = require('fs-extra');
+const firebase = require('firebase');
+const debug = require('debug');
+const webpack = require('webpack');
+const react_redux = require('react-redux');
+const jest = require('jest');
+const jsonwebtoken = require('jsonwebtoken');
+const helmet = require('helmet');
+const sequelize = require('sequelize');
+const commander = require('commander');
+
+// Get information about an Ethereum transaction receipt
+const txHash = '0xtransactionHash';
+web3.eth.getTransactionReceipt(txHash).then(receipt => {
+  console.log('Transaction receipt:', receipt);
+}).catch(err => {
+  console.error('Error getting transaction receipt:', err);
+});
+
+function sum(...args) {
+  return args.reduce((previous, current) => {
+    return previous + current;
+  });
+}
+console.log(sum(1, 2, 3));
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.question('Enter your name: ', name => {
+  console.log(`Hello, ${name}!`);
+  readline.close();
+});
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+server.on('message', (msg, rinfo) => {
+  console.log(`Message from client: ${msg.toString()}`);
+  console.log(`Client info: ${rinfo.address}:${rinfo.port}`);
+});
+server.bind(8080);
+
+const findMax = (numbers) => {
+  return Math.max(...numbers);
+};
+console.log(findMax([1, 3, 2, 8, 5]));
+
+const buffer1 = Buffer.from('Hello');
+const buffer2 = Buffer.from('Node.js');
+const buffer3 = Buffer.concat([buffer1, buffer2]);
+console.log('Concatenated buffer:', buffer3.toString());
+
+// Get the number of uncles for a block in Ethereum by block hash
+web3.eth.getBlockUncleCount('0xblockHash').then(uncles => {
+  console.log('Number of uncles by block hash:', uncles);
+}).catch(err => {
+  console.error('Error getting uncles by block hash:', err);
+});
